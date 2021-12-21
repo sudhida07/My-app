d6bdd4aef4c0fc5dba0f7e01379327896b8118ff
sudhida07 <sudhida07@gmail.com>
2021-12-21 18:04:05 +0530

    testdata

---
+++ src/module1.mjs
@@ -0,0 +1,8 @@
+const a="sudheer";
+const b="suresh";
+const c="divya";
+const d='sujatha';
+export default d;
+export {a};
+export {b};
+export {c};
---
+++ src/module2.mjs
@@ -0,0 +1,3 @@
+import dz,{a,b,c} from './module1.mjs'
+console.log(dz);
+console.log(a);console.log(b);console.log(c);
